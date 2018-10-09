779166df46d2c96830cd0ff5dc30860d84d69884
chuntley56 <coreyhuntley562@gmail.com>
2018-10-09 16:38:56 -0700

    Updates

---
+++ precache-manifest.c5288611aab07a5b06b79af10611332f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24b9915b9592fab05c1a",
+    "url": "/stock-quote/static/js/runtime~main.24b9915b.js"
+  },
+  {
+    "revision": "b5e9b5e8af0ff0971549",
+    "url": "/stock-quote/static/js/main.b5e9b5e8.chunk.js"
+  },
+  {
+    "revision": "d99e9afe884fc627a099",
+    "url": "/stock-quote/static/js/1.d99e9afe.chunk.js"
+  },
+  {
+    "revision": "b5e9b5e8af0ff0971549",
+    "url": "/stock-quote/static/css/main.05077c0a.chunk.css"
+  },
+  {
+    "revision": "92099511f04154b7a0df02d56ef7d23b",
+    "url": "/stock-quote/index.html"
+  }
+];
