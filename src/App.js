18e13668138c579bb39395c3802a2404e3699c77
chuntley56 <coreyhuntley562@gmail.com>
2018-10-22 14:23:11 -0700

    fecth popularity

--- src/App.js
+++ src/App.js
@@ -16,43 +16,18 @@ class StockChart extends Component {
     };
   }
 
- //  componentDidMount() {
- //     // const stockQuoteUrl = 'https://api.robinhood.com/quotes/?symbols=FB';
- //     // fetch(stockQuoteUrl)
- //     // .then(data => {return data.json()})
- //     //  .then(results => this.setState({ bid: results.results[0].bid_price, ask: results.results[0].ask_price, instrument: results.results[0].instrument.split('instruments/')[1].replace(/\/$/, "")}));
- //
- // const popularityUrl = '/instruments/popularity/?ids=ebab2398-028d-4939-9f1d-13bf38f81c50';
- //
- //     fetch(popularityUrl)
- //     // .then(data => {return data.json()})
- //     .then(results => console.log(results))
- //
- //
- //
- //  }
+  componentDidMount() {
+    const stockQuoteUrl = 'https://api.robinhood.com/quotes/?symbols=FB';
+    fetch(stockQuoteUrl)
+    .then(data => {return data.json()})
+     .then(results => this.setState({ bid: results.results[0].bid_price, ask: results.results[0].ask_price, instrument: results.results[0].instrument.split('instruments/')[1].replace(/\/$/, "")}));
+    }
 
-
-
-componentDidMount() {
-    fetch('/instruments/popularity/?ids=ebab2398-028d-4939-9f1d-13bf38f81c50')
-      .then(resp => {
-        console.log('======success=======');
-        resp.blob().then(re => {
-          var FR = new FileReader();
-          FR.onload = event => {
-            console.log(FR.result);
-          };
-          FR.readAsText(re);
-        });
-      })
-      .catch(err => {
-        console.log('======failure=======');
-        console.log(err);
-      });
-  }
-
-
+    componentDidUpdate() {
+      fetch(`/instruments/popularity/?ids=${this.state.instrument}`)
+      .then(data => {return data.json()})
+      .then(results => this.setState({ popularity: results.results[0].num_open_positions }) );
+    }
 
 
   //
@@ -60,7 +35,7 @@ componentDidMount() {
   //   const popularityUrl = `https://api.robinhood.com/instruments/popularity/?ids=${this.state.instrument}`;
   //   fetch('https://api.robinhood.com/instruments/popularity/?ids=ebab2398-028d-4939-9f1d-13bf38f81c50')
   //    .then(data => {return data.json()})
-  //    // .then(results => this.setState({ popularity: results.results[0].num_open_positions}))
+  //    .then(results => this.setState({ popularity: results.results[0].num_open_positions}))
   //         .then(results => console.log(results.results[0].num_open_positions))
   // }
 
